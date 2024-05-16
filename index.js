0b6920c9acf78fde42765bb49aab5351ee291d0d
sd54fd <rachaelrapsg@hotmail.com>
2024-05-16 07:25:59 +0700

    test commit

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-function isPrime(n) {
-  if (n <= 1) return false;
-  if (n <= 3) return true;
-  if (n % 2 === 0 || n % 3 === 0) return false;
-  let i = 5;
-  while (i * i <= n) {
-    if (n % i === 0 || n % (i + 2) === 0) return false;
-    i += 6;
+function coinChange(coins, amount) {
+  const dp = new Array(amount + 1).fill(Infinity);
+  dp[0] = 0;
+  for (const coin of coins) {
+    for (let i = coin; i <= amount; i++) {
+      dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+    }
   }
-  return true;
+  return dp[amount] === Infinity ? -1 : dp[amount];
 }
